e9123a4273fa837724f7a00ff391a354f3a85828
Maxine <mwandamaxyine@gmail.com>
2020-08-28 02:02:20 +0300

    added truth or dare questions

---
+++ db.sql
@@ -0,0 +1,50 @@
+package
+main
+
+CREATE DATABASE Truth_or_dare;
+USE Truth_or_dare;
+CREATE TABLE players
+(
+    user_id     INT(10) PRIMARY KEY AUTO_INCREMENT,
+    telegram_id INT(15)     NOT NULL,
+    first_name  VARCHAR(30) NOT NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE game_session
+(
+    game_id    INT(15) PRIMARY KEY AUTO_INCREMENT,
+    active     BOOL,
+    updated_at DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE player_scores
+(
+    player_score_id INT(30) PRIMARY KEY AUTO_INCREMENT,
+    user_id         INT,
+    game_id         INT,
+    scores          INT(30) DEFAULT 0,
+
+    FOREIGN KEY (user_id) REFERENCES players (user_id),
+    FOREIGN KEY (game_id) REFERENCES game_session (game_id)
+);
+
+CREATE TABLE truths_dares (
+    id        INT(10) PRIMARY KEY AUTO_INCREMENT,
+    challenge VARCHAR(256) NOT NULL,
+    type      ENUM ('truth', 'dare')
+);
+
+INSERT INTO truths_dares (challenge, type)
+VALUES
+('What are your top three turn-ons?', 'truth'),
+('What is your deepest darkest fear?', 'truth'),
+('Tell me about your first kiss', 'truth'),
+('Who is the sexiest person here?', 'truth'),
+('What is your biggest regret?', 'truth'),
+('Who here has the nicest butt?', 'truth'),
+('Who is your crush?', 'truth'),
+('Who was the last person you licked?', 'truth');
